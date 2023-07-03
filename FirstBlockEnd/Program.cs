cac904f0ee26455299b78abce94031d320358738
unknown <vladislavsafarov04@mail.ru>
2023-07-03 22:51:36 +0300

    Добавили метод по созданию нового массива из существующего, с элементами, длина которых меньше 4

--- FirstBlockEnd/Program.cs
+++ FirstBlockEnd/Program.cs
@@ -9,3 +9,13 @@
     }
     return arr;
 }
+
+string[] ArrayElemLessThanThree(string[] arr)
+{
+    string[] newArray = new string[arr.Length];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i].Length >= 3) newArray[i] = arr[i];
+    }
+    return newArray;
+}
