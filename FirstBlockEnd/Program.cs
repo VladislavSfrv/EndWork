477c06f42ee680b59f3334614f2ed4c26e907370
unknown <vladislavsafarov04@mail.ru>
2023-07-03 22:50:32 +0300

    Добавили метод по созданию массива строк

---
+++ FirstBlockEnd/Program.cs
@@ -0,0 +1,11 @@
+﻿string[] EnterArray(int n)
+{
+    string[] arr = new string[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        string temp = Console.ReadLine();
+        arr[i] = temp;
+    }
+    return arr;
+}
